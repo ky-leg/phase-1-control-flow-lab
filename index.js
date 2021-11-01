d6c7b36ec4247f8974f058f267ccd3888e2738c0
Kyle Goodwin <kyle.goodwin93@gmail.com>
2021-11-01 19:13:03 -0400

    always feels good finishign a labgit add .

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x < 400) {
+    return "This one is on me!"
+  } 
+  else if (x > 2000 && x <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (x > 2500) 
+    return "No can do."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const isNYC = city === "NYC" ? ("Ok, sounds good.") : ("No go.")
+  return isNYC
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  }
+
+  else if (tip === "not as generous"){
+    return "Thank you."
+  }
+
+  else {
+    return "Bye."
+  }
 }
+
+console.log(switchOnCharmFromTip("wut"))
